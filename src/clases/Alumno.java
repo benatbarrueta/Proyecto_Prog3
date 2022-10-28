89572cd3aca2c7de6787fd1f0a853e9ffb9bb21a
Beñat Barrueta <benat.barrueta@opendeusto.es>
2022-10-28 13:19:20 +0200

    terminar clases

--- src/clases/Alumno.java
+++ src/clases/Alumno.java
@@ -1,5 +1,42 @@
 package clases;
 
 public class Alumno extends Usuarios {
+	protected String email;
+	protected String curso;
+	
+	public Alumno(String nombre, String apellidos, int edad, String direccion, boolean alumno, String nombreUsuario,
+			String email, String curso) {
+		super(nombre, apellidos, edad, direccion, alumno, nombreUsuario);
+		this.email = email;
+		this.curso = curso;
+	}
+	
+	public Alumno(String nombre, String apellidos, int edad, String direccion, boolean alumno, String nombreUsuario) {
+		super(nombre, apellidos, edad, direccion, alumno, nombreUsuario);
+		this.email = "";
+		this.curso = "";
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", email" + email;
+	}
+	
 	
 }
--- src/clases/Asignatura.java
+++ src/clases/Asignatura.java
@@ -1,5 +1,81 @@
 package clases;
 
-public class Asignatura {
+import java.util.ArrayList;
 
+public class Asignatura {
+	// protected final String apuntes;
+	
+	protected String nombre;
+	protected Profesor profesor;
+	protected ArrayList<Alumno> alumnos;
+	protected ArrayList<Tarea> tareas;
+	protected double calificacion;
+	
+	public Asignatura(String nombre, Profesor profesor, ArrayList<Alumno> alumnos, ArrayList<Tarea> tareas,
+			double calificacion) {
+		super();
+		this.nombre = nombre;
+		this.profesor = profesor;
+		this.alumnos = alumnos;
+		this.tareas = tareas;
+		this.calificacion = calificacion;
+	}
+	
+	public Asignatura() {
+		super();
+		this.nombre = "";
+		this.profesor = new Profesor();
+		this.alumnos = new ArrayList<Alumno>();
+		this.tareas = new ArrayList<Tarea>();
+		this.calificacion = 0;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Profesor getProfesor() {
+		return profesor;
+	}
+
+	public void setProfesor(Profesor profesor) {
+		this.profesor = profesor;
+	}
+
+	public ArrayList<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(ArrayList<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
+
+	public ArrayList<Tarea> getTareas() {
+		return tareas;
+	}
+
+	public void setTareas(ArrayList<Tarea> tareas) {
+		this.tareas = tareas;
+	}
+
+	public double getCalificacion() {
+		return calificacion;
+	}
+
+	public void setCalificacion(double calificacion) {
+		this.calificacion = calificacion;
+	}
+
+	@Override
+	public String toString() {
+		return "Nombre: " + nombre + ", profesor=" + profesor + ", alumnos=" + alumnos + ", tareas=" + tareas
+				+ ", calificacion=" + calificacion + "]";
+	}
+	
+	
+	
 }
