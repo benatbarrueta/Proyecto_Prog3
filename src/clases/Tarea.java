768cf723e80bd585d548917f2b955946a70269c8
borjaocerin <borja.ocerinjauregui@opendeusto.es>
2022-10-29 18:16:12 +0200

    Clase tarea

--- src/clases/Tarea.java
+++ src/clases/Tarea.java
@@ -4,7 +4,42 @@ public class Tarea {
 	
 	protected Integer fecha_inicio;
 	protected Integer fecha_fin;
-	protected Integer calificacion;
+	protected String calificacion;
 	protected String nombre;
+	public Tarea(Integer fecha_inicio, Integer fecha_fin, String calificacion, String nombre) {
+		super();
+		this.fecha_inicio = fecha_inicio;
+		this.fecha_fin = fecha_fin;
+		this.calificacion = calificacion;
+		this.nombre = nombre;
+	}
+	public Integer getFecha_inicio() {
+		return fecha_inicio;
+	}
+	public void setFecha_inicio(Integer fecha_inicio) {
+		this.fecha_inicio = fecha_inicio;
+	}
+	public Integer getFecha_fin() {
+		return fecha_fin;
+	}
+	public void setFecha_fin(Integer fecha_fin) {
+		this.fecha_fin = fecha_fin;
+	}
+	public String getCalificacion() {
+		return calificacion;
+	}
+	public void setCalificacion(String calificacion) {
+		this.calificacion = calificacion;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	@Override
+	public String toString() {
+		return "Tarea: Nombre:" +nombre+ "Fecha inicio=" + fecha_inicio + ", Fecha fin:" + fecha_fin + ", Calificacion:" + calificacion ;
+	}
 
 }
