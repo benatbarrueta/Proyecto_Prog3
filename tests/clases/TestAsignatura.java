fa5e9bc48e2965f2fcbc45620b68e545fac4462d
Beñat Barrueta <benat.barrueta@opendeusto.es>
2022-10-30 12:01:28 +0100

    TestAsignaturas terminado

--- tests/clases/TestAsignatura.java
+++ tests/clases/TestAsignatura.java
@@ -12,71 +12,98 @@ public class TestAsignatura {
 	protected Profesor profesor = new Profesor();
 	protected ArrayList<Alumno> alumnos =new ArrayList<Alumno>();
 	protected ArrayList<Tarea> tareas= new ArrayList<Tarea>();
-	protected double calificacion;
 
 	@Test
 	public void testAsignaturaStringProfesorArrayListOfAlumnoArrayListOfTareaDouble() {
-		fail("Not yet implemented");
+		assertNotNull(asignatura);
+		assertEquals(asignatura.getNombre(), nombre);
+		assertEquals(asignatura.getProfesor(), profesor);
+		assertEquals(asignatura.getAlumnos(), alumnos);
+		assertEquals(asignatura.getTareas(), tareas);
+		assertEquals(asignatura.getCalificacion(), 0,0);
 	}
 
 	@Test
 	public void testAsignatura() {
-		fail("Not yet implemented");
+		Asignatura asignatura2 = new Asignatura();
+		assertNotNull(asignatura2);
+		assertEquals(asignatura.getNombre(), "");
+		assertEquals(asignatura.getProfesor(), new Profesor());
+		assertEquals(asignatura.getAlumnos(), new ArrayList<Alumno>());
+		assertEquals(asignatura.getTareas(), new ArrayList<Tarea>());
+		assertEquals(asignatura.getCalificacion(), 0,0);
 	}
 
 	@Test
 	public void testGetNombre() {
-		fail("Not yet implemented");
+		assertEquals(asignatura.getNombre(), nombre);
 	}
 
 	@Test
 	public void testSetNombre() {
-		fail("Not yet implemented");
+		String newNombre = "nombre2";
+		assertEquals(asignatura.getNombre(), nombre);
+		asignatura.setNombre(newNombre);
+		assertEquals(asignatura.getNombre(), newNombre);
 	}
 
 	@Test
 	public void testGetProfesor() {
-		fail("Not yet implemented");
+		assertEquals(asignatura.getProfesor(), profesor);
 	}
 
 	@Test
 	public void testSetProfesor() {
-		fail("Not yet implemented");
+		Profesor newProfesor = new Profesor();
+		assertEquals(asignatura.getProfesor(), profesor);
+		asignatura.setProfesor(newProfesor);
+		assertEquals(asignatura.getProfesor(), newProfesor);
 	}
 
 	@Test
 	public void testGetAlumnos() {
-		fail("Not yet implemented");
+		assertEquals(asignatura.getAlumnos(), alumnos);
 	}
 
 	@Test
 	public void testSetAlumnos() {
-		fail("Not yet implemented");
+		ArrayList<Alumno> newAlumnos = new ArrayList<Alumno>();
+		assertEquals(asignatura.getAlumnos(), alumnos);
+		asignatura.setAlumnos(newAlumnos);
+		assertEquals(asignatura.getAlumnos(), newAlumnos);
 	}
 
 	@Test
 	public void testGetTareas() {
-		fail("Not yet implemented");
+		assertEquals(asignatura.getTareas(), tareas);
 	}
 
 	@Test
 	public void testSetTareas() {
-		fail("Not yet implemented");
+		ArrayList<Tarea> newTarea = new ArrayList<Tarea>();
+		assertEquals(asignatura.getTareas(), tareas);
+		asignatura.setTareas(newTarea);
+		assertEquals(asignatura.getTareas(), newTarea);
 	}
 
 	@Test
 	public void testGetCalificacion() {
-		fail("Not yet implemented");
+		assertEquals(asignatura.getCalificacion(), 0,0);
 	}
 
 	@Test
 	public void testSetCalificacion() {
-		fail("Not yet implemented");
+		Double newCalificacion = 8.0;
+		assertEquals(asignatura.getCalificacion(), 0,0);
+		asignatura.setCalificacion(newCalificacion);
+		assertEquals(asignatura.getCalificacion(), 8,0);
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		String toString = "Nombre " + profesor + alumnos + tareas + "calificacion";
+		
+		assertEquals(asignatura.toString(), toString);
 	}
 
 }
