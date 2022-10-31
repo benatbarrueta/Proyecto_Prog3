526f751673e71d0bbbc742a313c78cdf59d87566
borjaocerin <borja.ocerinjauregui@opendeusto.es>
2022-10-31 10:26:37 +0100

    pobrando consas en la sql

--- src/clases/GestorBD.java
+++ src/clases/GestorBD.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.sql.*;
 public class GestorBD {
 
 	protected static final String DRIVER_NAME = "org.sqlite.JDBC";
