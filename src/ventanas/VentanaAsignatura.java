dab8722d474f1c24db7541fc9dff58ad7d5633b9
Iker <iker.gonzalez@opendeusto.es>
2022-12-05 13:31:11 +0100

    VENTANA ASIGNATURA

--- src/ventanas/VentanaAsignatura.java
+++ src/ventanas/VentanaAsignatura.java
@@ -5,6 +5,8 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,11 +20,23 @@ public class VentanaAsignatura extends JFrame{
 	protected JLabel tareas;
 	protected JLabel nombreAsig;
 	protected JLabel apuntes;
+	protected JLabel fecha;
+	protected int hora;
+	protected int minutos;
+	protected int segundos;
+	
+	
+	Calendar calendario = Calendar.getInstance();
+	
+	
+	
 	
 	protected JButton tareaBoton;
 	public VentanaAsignatura(Object objeto, String tipo, Gestor gestor, Asignatura asignatura) {
 		Container cp = this.getContentPane();
 		
+		
+		//DIFERENCIAR SI ENTRA UN ALUMNO O PROFESOR
 		if (tipo == "Alumno") {
 			Alumno alumno = (Alumno) objeto;
 			
@@ -30,24 +44,54 @@ public class VentanaAsignatura extends JFrame{
 			Profesor profesor = (Profesor) objeto;
 			
 		}
-		
+		// CREACIONES
 		cp.setLayout(new BorderLayout());
+		
 		
 		JPanel centro = new JPanel();
 		JPanel norte = new JPanel();
 		tareas = new JLabel("Tareas");
 		nombreAsig = new JLabel("Asignatura:  " + asignatura.getNombre());
 		apuntes = new JLabel("Apuntes");
+		Calendar calendario = new GregorianCalendar();
+		boolean ventana = true;
 		
-	
+			
+		
+		
+		
+		
+		hora = calendario.get(Calendar.HOUR_OF_DAY);
+		minutos = calendario.get(Calendar.MINUTE);
+		segundos = calendario.get(Calendar.SECOND);
+		
+		
+		
+		Thread hilo = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				fecha = new JLabel("Fecha:  "+ hora + ":" + minutos+":" + segundos);
+				
+			}
+			
+		});
+		hilo.start();
+		 
+		
+		
+		
+		//ADDS
 		cp.add(centro, BorderLayout.CENTER);
 		cp.add(norte, BorderLayout.NORTH);
 		
 		
 		norte.setLayout(new GridLayout(1,1));
 		centro.setLayout(new GridLayout(8,2));
+		
 		//NORTE
 		norte.add(nombreAsig);
+		norte.add(fecha);
 		
 		
 		//CENTRO
