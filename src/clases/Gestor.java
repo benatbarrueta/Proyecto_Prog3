5e2cafcaa65553e4310e53be8d2f162bcff45f9a
borjaocerin <borja.ocerinjauregui@opendeusto.es>
2023-01-13 20:54:38 +0100

    qweqweq

--- src/clases/Gestor.java
+++ src/clases/Gestor.java
@@ -350,114 +350,7 @@ PrintWriter pw = new PrintWriter("asignaturas.csv");
 	}
 	
 	 
-	/*
-		try (BufferedReader in = new BufferedReader(new FileReader(filename))){
-		
-			String linea;
-			StringTokenizer tokenizer;
-			Tarea tarea;
-			
-			in.readLine(); // Saltar linea cabezera
-			while((linea = in.readLine())!= null) {
-				tokenizer= new StringTokenizer(linea,"/n");
-			
-				tarea= new Tarea();
-				tarea.setEmailAlumno(tokenizer.nextToken());
-				tarea.setNombre(tokenizer.nextToken());		
-				tarea.setFecha_fin(tokenizer.nextToken());				
-				tarea.setCalificacion(Double.parseDouble(tokenizer.nextToken()));
-				String nomAsig = tokenizer.nextToken();
-				for (Asignatura a : asignaturas) {
-					if (a.getNombre().equals(nomAsig)) {
-						a.getTareas().add(tarea);
-					}
-				}
-				tareas = new ArrayList<Tarea>();
-				if (!tareas.contains(tarea) && tareas != null) {
-					tareas.add(tarea);
-				}
-				
-			}
-			//System.out.println(tareas);
-			
-			
-		}catch (Exception ex) {
-			// TODO: handle exception
-			System.err.println("Error en el main: " +ex);
-			ex.printStackTrace();
-		}
-
-	}*/
 	
-	
-/*	try  {	 	  	   
-		//System.out.println(filename);
-	     PrintWriter pw = new PrintWriter(filename);
-	     pw.println("Email Alumno" + ";" +"Nombre" + ";" + "Fecha" +";" + "Calificacion" + ";" + "Asignatura");
-	     
-//	     for (Asignatura a : asignaturas) {
-//			for (Tarea tarea : a.getTareas()) {
-	//    	 pw.println(
-	//    			 tarea.getEmailAlumno() + ";" + tarea.getNombre() + ";" + tarea.getFecha_fin()+ ";" +tarea.getCalificacion() +";" + a.nombre + ";"
-	//    			 );		
-	//		}
-		}
-	     
-	     
-	 //   pw.close(); 
-	     
-	} catch (Exception e) {
-		System.err.println(String.format("Error en el main: %s", e.getMessage()));
-		e.printStackTrace();
-	}
-	}
-	public void guardarAsignaturaBinario(String filename) {
-		try {
-			FileOutputStream fos= new FileOutputStream(filename);
-			
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			for (Asignatura asignatura : asignaturas) {
-				oos.writeObject(asignatura);
-			}
-			
-			oos.close();
-			fos.close();
-			
-			
-		} catch (IOException e ) {
-			// TODO: handle exception
-			System.err.println("Error guardar datos ");
-		}
-	}
-
-		
-	public void leerAsignaturasBinario(String filename) {
-		try {
-			FileInputStream fis= new FileInputStream(filename);
-			
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			
-			this.asignaturas= (ArrayList<Asignatura>) ois.readObject();
-			ois.close();
-			fis.close();
-								
-			
-		} catch (IOException  | ClassNotFoundException e ) {
-			// TODO: handle exception
-			System.err.println("Error cargar datos");
-		}
-
-	
-	}
-	public void CrearAsignaturas() {
-	Asignatura	asignatura;
-		for (int i = 0; i < 10; i++) {
-			String name = "asignatura" +i;
-/*			asignatura=new Asignatura(name,new Profesor(),new ArrayList<Alumno>(),new ArrayList<Tarea>(),i);*/
-	//		asignaturas.add(asignatura);
-	
-		//System.out.println(asignaturas);
-	//}
 	public ArrayList<Usuarios> initUsuarios() {
 		Usuarios	usuario;
 			for (int i = 0; i < 10; i++) {
@@ -494,28 +387,28 @@ PrintWriter pw = new PrintWriter("asignaturas.csv");
 		*/
 		
 		//CREAR PROFESORES, ALUMNOS, TAREAS Y ASIGNATURAS
-		ArrayList<Profesor> profesores = GestorBD.gestorBD.obtenerDatosProfesor();
+	//	ArrayList<Profesor> profesores = GestorBD.gestorBD.obtenerDatosProfesor();
 		ArrayList<Alumno> alumnos = GestorBD.gestorBD.obtenerDatosAlumnos();
 		//ArrayList<Tarea> tareas = GestorBD.gestorBD.obtenerDatosTareas();
 		//ArrayList<Asignatura> asignaturas = GestorBD.gestorBD.obtenerDatosAsignaturas();
 		
 
 		
-		if (profesores.size() == 0) {
+		/*if (profesores.size() == 0) {
 			for (int i = 0; i < 2; i++) {
-				Profesor profesor = new Profesor();
-				profesor.setContraseña(i+"");
-				profesor.setApellidos("Apellido"+i);
-				profesor.setDireccion("dir");
-				profesor.setEdad(i);
-				profesor.setNombre("nombre"+i);
-				profesor.setSalario(i);
-				profesor.setNombreUsuario("Profe");
-				gestor.profesor.add(profesor);		
+		//		Profesor profesor = new Profesor();
+			//			profesor.setContraseña(i+"");
+			//			profesor.setApellidos("Apellido"+i);
+			//			profesor.setDireccion("dir");
+			//			profesor.setEdad(i);
+			//			profesor.setNombre("nombre"+i);
+				//		profesor.setSalario(i);
+				//		profesor.setNombreUsuario("Profe");
+				//		gestor.profesor.add(profesor);		
 			}
 			
 			Profesor profesor = new Profesor("Borja", "OM", 5, "DIR","NU","", 0);
-			gestor.profesor.add(profesor);
+			gestor.profesor.add(profesor);*/
 	/*	}
 		if (alumnos.size() == 0) {
 			for (int i = 0; i < 5; i++) {
@@ -567,4 +460,4 @@ PrintWriter pw = new PrintWriter("asignaturas.csv");
 	
 }
 	
-	}
+	
