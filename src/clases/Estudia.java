199f097517c2f5902fe384b65308ba85a3183974
borjaocerin <borja.ocerinjauregui@opendeusto.es>
2023-01-13 18:47:17 +0100

    ew

---
+++ src/clases/Estudia.java
@@ -0,0 +1,24 @@
+package clases;
+
+public class Estudia {
+int id_alumno;
+int id_asignatura;
+public Estudia() {
+	super();
+	this.id_alumno = 0;
+	this.id_asignatura = 0;
+}
+public int getId_alumno() {
+	return id_alumno;
+}
+public void setId_alumno(int id_alumno) {
+	this.id_alumno = id_alumno;
+}
+public int getId_asignatura() {
+	return id_asignatura;
+}
+public void setId_asignatura(int id_asignatura) {
+	this.id_asignatura = id_asignatura;
+}
+
+}
